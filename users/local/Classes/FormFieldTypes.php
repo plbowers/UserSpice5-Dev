11dae7ee2a5ea7602a46560111db6e6d48d1aa3d
Peter L. Bowers <plbowers@gmail.com>
2017-05-20 15:21:22 +0200

    just some formatting to make it easy to read

--- users/local/Classes/FormFieldTypes.php
+++ users/local/Classes/FormFieldTypes.php
@@ -17,29 +17,33 @@ class FormField_MultiCheckbox extends FormField_Checklist {
     # unless you are deliberately separating the functionality of what was
     # originally designed to be a simple alias
 }
-class FormField_File          extends US_FormField_File {
+class FormField_File           extends US_FormField_File {
 }
-class FormField_Hidden        extends US_FormField_Hidden {
+class FormField_Hidden         extends US_FormField_Hidden {
 }
-class FormField_HTML          extends US_FormField_HTML {
+class FormField_HTML           extends US_FormField_HTML {
 }
-class FormField_MultiHidden   extends US_FormField_MultiHidden {
+class FormField_MultiHidden    extends US_FormField_MultiHidden {
 }
-class FormField_Password      extends US_FormField_Password {
+class FormField_Password       extends US_FormField_Password {
 }
-class FormField_Radio         extends US_FormField_Radio {
+class FormField_Radio          extends US_FormField_Radio {
 }
-class FormField_ReCaptcha     extends US_FormField_ReCaptcha {
+class FormField_ReCaptcha      extends US_FormField_ReCaptcha {
 }
-class FormField_SearchQ       extends US_FormField_SearchQ {
+class FormField_SearchQ        extends US_FormField_SearchQ {
 }
-class FormField_Select        extends US_FormField_Select {
+class FormField_Select         extends US_FormField_Select {
 }
-class FormField_Table         extends US_FormField_Table {
+class FormField_SigninFacebook extends US_FormField_SigninFacebook {
 }
-class FormField_TabToC        extends US_FormField_TabToC {
+class FormField_SigninGoogle   extends US_FormField_SigninGoogle {
 }
-class FormField_Text          extends US_FormField_Text {
+class FormField_Table          extends US_FormField_Table {
 }
-class FormField_Textarea      extends US_FormField_Textarea {
+class FormField_TabToC         extends US_FormField_TabToC {
+}
+class FormField_Text           extends US_FormField_Text {
+}
+class FormField_Textarea       extends US_FormField_Textarea {
 }
