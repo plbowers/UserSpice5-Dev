09ceac8ad0ec54cd7c003b314a0d1406f820f82e
Peter L. Bowers <plbowers@gmail.com>
2017-05-20 15:27:07 +0200

    first shot at a refactor - just making it more DRY basically

--- users/core/helpers/users_online.php
+++ users/core/helpers/users_online.php
@@ -5,72 +5,55 @@ function usersOnline () {
 }
 
 function ipCheck() {
-  if (getenv('HTTP_CLIENT_IP')) {
-    $ip = getenv('HTTP_CLIENT_IP');
-  }
-  elseif (getenv('HTTP_X_FORWARDED_FOR')) {
-    $ip = getenv('HTTP_X_FORWARDED_FOR');
-  }
-  elseif (getenv('HTTP_X_FORWARDED')) {
-    $ip = getenv('HTTP_X_FORWARDED');
-  }
-  elseif (getenv('HTTP_FORWARDED_FOR')) {
-    $ip = getenv('HTTP_FORWARDED_FOR');
-  }
-  elseif (getenv('HTTP_FORWARDED')) {
-    $ip = getenv('HTTP_FORWARDED');
-  }
-  else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-  }
-  return $ip;
+    $ip = false;
+    foreach (['HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED'] as $env) {
+        if ($ip = getenv($env)) {
+            break;
+        }
+    }
+    if (!$ip) {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
 }
 
 function new_user_online($user_id=0) {
+    #dbg("new_user_online($user_id): Entering");
     global $T;
 	$db = DB::getInstance();
 	$ip = ipCheck();
 	$timestamp = time();
 	$checkUserId = $db->query("SELECT id FROM $T[users_online] WHERE user_id = ?",array($user_id));
 	$countUserId = $checkUserId->count();
+	$fields = [ 'timestamp'=>$timestamp, 'ip'=>$ip, 'user_id'=>$user_id ];
 
-	if($countUserId == 0){
-		$fields =array('timestamp'=>$timestamp, 'ip'=>$ip,'user_id'=>$user_id);
-		$db->insert('users_online',$fields);
-	}else{
-		if($user_id==0){
-			$fields =array('timestamp'=>$timestamp, 'ip'=>$ip,'user_id'=>$user_id);
+	if ($countUserId == 0) {
+		$db->insert('users_online', $fields);
+	} else {
+		if ($user_id==0) {
 			$checkQ = $db->query("SELECT id FROM $T[users_online] WHERE user_id = 0 AND ip = ?",array($ip));
-			if($checkQ->count()==0){
-				$db->insert('users_online',$fields);
-			}else{
+			if ($checkQ->count()==0) {
+				$db->insert('users_online', $fields);
+			} else {
 				$to_update = $checkQ->first();
-				$db->update('users_online',$to_update->id,$fields);
+				$db->update('users_online', $to_update->id, $fields);
 			}
-			//Not sure why this code is being called here
-			//$to_update = $checkQ->first();
-			//$db->update('users_online',$to_update->id,$fields);
-		}else{
-			$fields =array('timestamp'=>$timestamp, 'ip'=>$ip,'user_id'=>$user_id);
-			#$checkQ = $db->query("SELECT id FROM $T[users_online] WHERE user_id = ?",array($user_id));
+		} else {
 			$to_update = $checkUserId->first();
-			$db->update('users_online',$to_update->id,$fields);
+			$db->update('users_online', $to_update->id, $fields);
 		}
 	}
 }
 
-function delete_user_online() {
-  $db = DB::getInstance();
-  $timeout = 86400; //30 minutes - This can be changed
-  $timestamp = time();
-  $delete = $db->query("DELETE FROM $T[users_online] WHERE timestamp < ($timestamp - $timeout)");
+# Delete rows in `users_online` that were updated more then $timeout seconds ago
+function delete_user_online($timeout = 86400) {
+    global $T;
+    $db = DB::getInstance();
+    $delete = $db->query("DELETE FROM $T[users_online] WHERE timestamp < (UNIX_TIMESTAMP() - $timeout)");
 }
 
-function count_users() {
+function count_users($timeout = 1800) {
+    global $T;
     $db = DB::getInstance();
-    $timestamp = time();
-	  $timeout = 1800; //30 minutes - This can be changed
-    $selectAll = $db->query("SELECT * FROM $T[users_online] WHERE timestamp > ($timestamp-$timeout)");
-    $count = $selectAll->count();
-    return $count;
+    return $db->query("SELECT COUNT(*) AS c FROM $T[users_online] WHERE timestamp > (UNIX_TIMESTAMP()-$timeout)")->first()->c;
 }
