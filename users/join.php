69df010b3a823a9d7628c3892c1a67a1cfcb17e8
Peter L. Bowers <plbowers@gmail.com>
2017-05-20 19:50:02 +0200

    should not be part of git

--- users/join.php
+++
@@ -1,5 +0,0 @@
-<?php
-$formName = 'join.php';
-#$enableMasterHeaders = $enableMasterFooters = true;
-require_once 'C:/wamp/www/UserSpice5-Dev/users/z_us_root.php';
-require_once US_ROOT_DIR.'core/master_form.php';
